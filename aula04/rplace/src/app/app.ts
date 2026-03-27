6686e88f79aeeee381fb24d9e2cd052d66a1c8d0
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2026-03-27 16:49:35 -0300

    Atualização

--- aula04/rplace/src/app/app.ts
+++ aula04/rplace/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
@@ -13,10 +13,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('rplace');
-  protected _date = signal(new Date())
+  protected _date = signal(new Date());
 
+  protected _stringDate: Signal<string> = computed(() => {
+    return `${this._date().getFullYear()}-
+      ${this._date().getMonth()+1}-
+      ${this._date().getDate()}`
+  })
 
   ngOnInit() {
     console.log(this.title());
+  }
+
+  addDay(add: boolean = true) {
+    this._date.update((oldValue) => {
+      let day = add ? oldValue.getDate()+1 : oldValue.getDate()-1;
+      let month = oldValue.getMonth();
+      let year = oldValue.getFullYear();
+
+      return new Date(year, month, day);
+    })
   }
 }
