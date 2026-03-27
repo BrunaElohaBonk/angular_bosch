f018668edd51f6da74ecca0b3a9e5155696f9319
BrunaElohaBonk <brunaelohabonk698@gmail.com>
2026-03-27 16:19:24 -0300

    Atualização

---
+++ aula04/rplace/src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('rplace');
+  protected _date = signal(new Date())
+
+
+  ngOnInit() {
+    console.log(this.title());
+  }
+}
